f1042af2731a2427d834e6f4d40d33d81eb66d22
ali hatem <AliHatem>
2023-01-15 16:05:38 +0200

    update Api yo accept limit variable

--- src/Pages/Category/AllCategoryPage.js
+++ src/Pages/Category/AllCategoryPage.js
@@ -2,12 +2,29 @@ import React, { useEffect } from "react";
 import { Container } from "react-bootstrap";
 import CategoryContainer from "../../Components/Category/CategoryContainer";
 import PaginationCompontent from "../../Components/Utility/Pagination";
-
+import getAllCategory from "../../redux/actions/categoryAction";
+import { useDispatch, useSelector } from "react-redux";
 const AllCategoryPage = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllCategory(2));
+  }, []);
+
+  const category = useSelector((state) => state.categoryReducer.categories);
+  const loading = useSelector((state) => state.categoryReducer.isLoding);
+
+  let pageNumbers = 0;
+  if (category.paginationResult) {
+    pageNumbers = category.paginationResult.numberOfPages;
+  }
+
   return (
     <Container style={{ minHeight: "670px" }}>
-      <CategoryContainer />
-      <PaginationCompontent />
+      <CategoryContainer data={category.data} loading={loading} />
+      <PaginationCompontent
+        pageNumbers={2}
+        paginationPageNumber={pageNumbers}
+      />
     </Container>
   );
 };
--- src/redux/actions/categoryAction.js
+++ src/redux/actions/categoryAction.js
@@ -1,8 +1,8 @@
 import { GET_ALL_CATEGORY, GET_ERROR } from "../types/categorytypes";
 import useGetData from "../../hook/useGetData";
-const getAllCategory = () => async (dispatch) => {
+const getAllCategory = (limit) => async (dispatch) => {
   try {
-    const response = await useGetData("/api/v1/categories");
+    const response = await useGetData(`/api/v1/categories?limit=${limit}`);
     dispatch({
       type: GET_ALL_CATEGORY,
       payload: response.data,
