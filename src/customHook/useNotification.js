584951f220f36c8f6da02188d0395a046988f349
ali hatem <AliHatem>
2023-01-16 14:42:30 +0200

    recator code create custom hook "Add-category-hook" handle listing to data on addCatigory component

---
+++ src/customHook/useNotification.js
@@ -0,0 +1,10 @@
+// //  to make notification in any component
+// const notify = (message, type) => {
+//   if (type === "success") {
+//     toast.success(message, {});
+//   } else if (type === "error") {
+//     toast.error(message, {});
+//   }
+// };
+
+// export default notify;
